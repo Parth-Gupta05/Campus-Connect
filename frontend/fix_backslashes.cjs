ce4af59b2e8f9069ecca85b85af2f558a3987d98
T4RuN05 <tarunasthana2005@gmail.com>
2026-09-24 02:30:35 +0530

    chore: Remove old frontend refactoring scripts

--- frontend/fix_backslashes.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let file = fs.readFileSync('src/components/profile/VerificationSection.jsx', 'utf8');
-
-file = file.replace(/\\`@\\\$\{(.*?)\}\\`/g, '`@${$1}`');
-file = file.replace(/\\`@\$\{(.*?)\}\\`/g, '`@${$1}`');
-
-fs.writeFileSync('src/components/profile/VerificationSection.jsx', file);
-
-console.log('Fixed VerificationSection backslashes');
